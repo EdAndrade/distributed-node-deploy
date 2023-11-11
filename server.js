428655714d932bdea346844f462eed6cc7684017
edAndrade <edmilisonwork@gmail.com>
2023-11-11 15:53:16 +0100

    Application files

---
+++ server.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const server = require('fastify')();
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 8000;
+const Recipe = require('./recipe.js');
+
+server.get('/', async (req, reply) => {
+    return "Hello from Distributed Node.js!";
+});
+
+server.get('/recipes/:id', async (req, reply) => {
+    const recipe = new Recipe(req.params.id);
+    await recipe.hydrate();
+    return recipe;
+});
+
+server.listen(PORT, HOST, (err, host) => {
+    console.log(`Server running at ${host}`);
+});
